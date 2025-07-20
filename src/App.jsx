e7f04d9477c604d4adab68a77f7fa940086b5a55
AryamanRathr0re <aryanrathore301@gmail.com>
2025-07-21 01:52:01 +0530

    feat: integrate Tailwind CSS and DaisyUI, add routing and new components
    - Added Tailwind CSS and DaisyUI dependencies to package.json
    - Updated App component to include React Router for navigation
    - Created Body component to serve as a layout wrapper with Navbar and Footer
    - Implemented Navbar component with dropdown for user options
    - Added Footer component with navigation links and social media icons
    - Created Login and Profile components as placeholders for future development
    - Removed existing CSS styles in index.css and replaced with Tailwind imports
    - Updated vite.config.js to include Tailwind CSS plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Body from "./components/Body";
 
 function App() {
-
   return (
     <>
-    <h1>Hello World</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <Body />
+              </div>
+            }
+          >
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {/* <Navbar /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
