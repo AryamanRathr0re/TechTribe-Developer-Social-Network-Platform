4786c7f550cb8d5e46f19d7a0a5889f9d4e16ab8
AryamanRathr0re <aryanrathore301@gmail.com>
2025-12-23 22:43:22 +0530

    minor bugs

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,18 @@ const PROD_URL = "https://techtribe-backend-node-js-express-api.onrender.com";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+      },
+    },
+  },
   server: {
     proxy: {
       "/api": {
